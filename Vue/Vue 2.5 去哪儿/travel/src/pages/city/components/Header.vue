ee8ebc5c4ca5e21469e434e93d1317d9026c2e0a
Angus Liu <angus.liu96@gmail.com>
2018-10-06 10:47:03 +0800

    feat: add vue & react

---
+++ Vue/Vue 2.5 去哪儿/travel/src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+    <div>城市选择</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl';
+
+.header {
+  .back-icon {
+    position: absolute;
+    float: left;
+    width: 0.64rem;
+    font-size: 0.4rem;
+    color: #fff
+  }
+
+  overflow: hidden;
+  height: $headerHight;
+  line-height: $headerHight;
+  text-align: center;
+  color: #fff;
+  background: $bgColor;
+  font-size: 0.32rem;
+}
+</style>
---
+++ Vue/Vue 2.5 去哪儿/travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1801/c9/c982eac885a4a1cfa3.img.jpg_600x330_775a58fc.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus">
+.banner {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 55%;
+
+  .banner-img {
+    width: 100%;
+  }
+}
+</style>
