c1cb29be13e45abe9e9e12fa4ebe08470c43892c
Angus <liuanguang@cvte.com>
2019-08-05 14:06:21 +0800

    feat: add kotlin in action.

---
+++ Kotlin/kotlin-in-action/src/chapter01/Person.kt
@@ -0,0 +1,10 @@
+package chapter01
+
+data class Person(val name: String,
+                  val age: Int? = null)
+
+fun main() {
+    val persons = listOf(Person("Angus", 23), Person("Bob"))
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("The oldest is $oldest")
+}
