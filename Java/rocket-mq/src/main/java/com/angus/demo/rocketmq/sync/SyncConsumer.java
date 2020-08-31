42722cd8c98687f0d4ec1fafeb0e4d4403e3041a
liuanguang <liuanguang@cvte.com>
2020-08-31 09:51:58 +0800

    feat: add sync demo

---
+++ Java/rocket-mq/src/main/java/com/angus/demo/rocketmq/sync/SyncConsumer.java
@@ -0,0 +1,33 @@
+package com.angus.demo.rocketmq.sync;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+
+import static com.angus.demo.rocketmq.sync.SyncProducer.*;
+
+/**
+ * 同步消费者
+ */
+public class SyncConsumer {
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("default_mq_push_consumer_group");
+        consumer.setNamesrvAddr(NAMESRV_ADDR);
+
+        // 指定从哪里开始
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        // 订阅待消费消息的主题和标签
+        consumer.subscribe(TOPIC, TAGS);
+
+        // 注册监听器处理从 broker 获取到的消息
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            System.out.println(Thread.currentThread().getName() + " Receive new message: " + msgs);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        consumer.start();
+    }
+
+}
---
+++ Java/rocket-mq/src/main/java/com/angus/demo/rocketmq/sync/SyncProducer.java
@@ -0,0 +1,32 @@
+package com.angus.demo.rocketmq.sync;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * 同步生产者
+ */
+public class SyncProducer {
+
+    final static String NAMESRV_ADDR = "172.20.220.133:9876";
+
+    final static String TOPIC = "TopicTest";
+
+    final static String TAGS = "TagA";
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("default_mq_producer_group");
+        producer.setNamesrvAddr(NAMESRV_ADDR);
+
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            Message message = new Message(TOPIC, TAGS, "Hello, Rocket MQ!".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(message, 2000 * 1000);
+            System.out.println(sendResult);
+        }
+        // 不再使用时需要关闭
+        producer.shutdown();
+    }
+}
