f488bba66cb2cd3584fac551f1475c26d6ef1152
liuanguang <liuanguang@cvte.com>
2020-10-10 12:21:17 +0800

    feat: update no-visibility demo

--- Java/java-concurrency-in-practice/src/main/java/ch03/se01/NoVisibility.java
+++ Java/java-concurrency-in-practice/src/main/java/ch03/se01/NoVisibility.java
@@ -1,23 +1,23 @@
 package ch03.se01;
 
 /**
- * 在没有同步的情况下共享变量
+ * 在没有同步的情况下共享变量出现不可见性
  */
 public class NoVisibility {
     private static boolean ready;
-    private static int number;
 
     private static class ReaderThread extends Thread {
         @Override
         public void run() {
-            while(!ready) Thread.yield();
-            System.out.println(number);
+            while (!ready) {
+                // 因可见性导致的死循环
+            }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new ReaderThread().start();
-        number = 42;
+        Thread.sleep(100);
         ready = true;
     }
 }
