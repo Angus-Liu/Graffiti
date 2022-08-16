f18a4f553384ca3fa17d5642f041cb31a5194cac
Angus Liu <angus.liu96@gmail.com>
2022-08-16 16:06:44 +0800

    feat: add data-types

--- Rust/data-types/src/main.rs
+++ Rust/data-types/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    for x in a {
-        println!("{x}");
-    }
+    // let a: [i32; 5] = [1, 2, 3, 4, 5];
+    // for x in a {
+    //     println!("{x}");
+    // }
+    //
+    // let b = ['x'; 5];
+    // for x in b {
+    //     println!("{x}");
+    // }
+    hello();
+}
+
+fn hello() {
+    println!("Hello, I'm Robot DIDIDADIDI! 🤖️");
 }
