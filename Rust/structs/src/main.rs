d6ae6b6af4d5f3f3b58f99293017dbe2382f6bdf
Angus Liu <angus.liu96@gmail.com>
2022-08-18 21:32:16 +0800

    feat: add structs

--- Rust/structs/src/main.rs
+++ Rust/structs/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        active: true,
+        username: String::from("Angus"),
+        email: String::from("angus.liu86@qq.com"),
+        sign_in_count: 1,
+    };
+    user1.email = String::from("test@qq.com");
+
+    let user2 = User {
+        email: String::from("user2@qq.com"),
+        ..user1
+    };
+    // println!("{}", user1.username);
+    println!("{}", user2.username);
+
+    let black = Color(0, 0, 0);
+    println!("{}", black.0);
+}
+
+struct Color(i32, i32, i32);
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
